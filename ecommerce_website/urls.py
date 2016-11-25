63645440b7e0b65d93b0e85731bb84902ed3e7b9
misodengaku <misodengaku@gmail.com>
2016-11-25 18:28:03 +0900

    import抜け

--- ecommerce_website/urls.py
+++ ecommerce_website/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from ecommerce.views import create_user_form, create_user
+from django.conf.urls.static import static
+from ecommerce_website import settings 
 
 urlpatterns = [
     url(r'^ecommerce/', include('ecommerce.urls')),
