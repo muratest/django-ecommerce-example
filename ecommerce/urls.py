3b74933f1b549b83fbb7bb021e45d0bc041bb2e5
Ubuntu User <y-matsuo@sakura.ad.jp>
2016-11-25 20:16:51 +0900

    add url

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^cart_list/$', views.cart_list, name='cart_list'),
-    #url(r'^cart_add/(?<>)$', views.cart_list, name='cart_list'),
-    #url(r'^cart_delete/$', views.cart_list, name='cart_list'),
-    #url(r'^cart_reset/$', views.cart_list, name='cart_list'),
+    url(r'^cart_add/(?P<product_id>[0-9]+)/$', views.cart_add, name='cart_add'),
+    url(r'^cart_delete/(?P<product_id>[0-9]+)/$', views.cart_delete, name='cart_delete'),
+    url(r'^cart_reset/$', views.cart_reset, name='cart_reset'),
     url(r'^order/$', views.order_form, name='order_form'),
     url(r'^order_execute/$', views.order_execute, name='order_execute'),
     url(r'^products/$', views.product_list, name='product_list'),
