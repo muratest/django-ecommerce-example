fe25203c2d1bf983ed0da090ed9a2a0fe169dba7
misodengaku <misodengaku@gmail.com>
2016-11-25 17:32:50 +0900

    オーダー内容の表示

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -8,15 +8,10 @@ def index(request):
 
 
 def order_form(request):
-    if request.method == 'POST':
-
-        # 注文をPOST
-
-        return HttpResponse('注文しました')
-
     # あとでセッションから一覧持ってくる
     products = Product.objects.all()
 
+    # 決済方法一覧を取得
     payments = Payment.objects.all()
 
     # オーダー画面
@@ -28,22 +23,25 @@ def order_execute(request):
     if request.method != 'POST':
         return HttpResponse('Bad request')
 
+    # 決済方法をPOSTリクエストから取得
     payment = Payment.objects.get(id=request.POST['payment'])
 
     # あとでセッションから一覧持ってくる
     products = Product.objects.all()
     customer = Customer.objects.get(first_name='a', last_name='a')
+
     payment = Payment.objects.get(id=int(request.POST['payment']))
 
+    # 注文作成
     order = Order.objects.create(customer=customer, payment=payment)
 
     for product in products:
-        # とりあえず1個
-        item_order = Order_Product(product=product, count=1, price=product.price, order=order)
+        # 注文対象商品をとりあえず1個ずつ持ってきて注文と関連づけ
+        item_order = Order_Product(
+            product=product, count=1, price=product.price, order=order)
         item_order.save()
 
-
-    return render(request, 'order_complete.html', {'products': 'products', 'payments': 'payments'})
+    return render(request, 'order_complete.html', {'products': products, 'payment': payment})
 
 def product_list(request):
     products = Product.objects.all()
